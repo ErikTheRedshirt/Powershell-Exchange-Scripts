5d5c1279c882a31854c07191d2eefecc7a4094cf
ErikTheRedshirt <97266008+ErikTheRedshirt@users.noreply.github.com>
2022-01-06 18:00:34 -0800

    Update Users with top recoverables folder.ps1

--- Users with top recoverables folder.ps1
+++ Users with top recoverables folder.ps1
@@ -1,2 +1,2 @@
 ﻿$count
-Get-Mailbox -ResultSize Unlimited -Filter "IsMailboxEnabled '$true'" | Get-MailboxFolderStatistics -FolderScope RecoverableItems | Sort-Object TotalItemSize -Descending | Select-Object DisplayName,TotalItemSize -First $count | Export-CSV .\top100mailboxes.csv
+Get-Mailbox -ResultSize Unlimited -Filter "IsMailboxEnabled '$true'" | Get-MailboxFolderStatistics -FolderScope RecoverableItems | Sort-Object TotalItemSize -Descending | Select-Object DisplayName,TotalItemSize -First $count | Export-CSV .\topmailboxes.csv
