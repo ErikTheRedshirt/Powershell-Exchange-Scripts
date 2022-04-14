a8366195108349eae1ce32d06ce631d6eb4c8fcc
ErikTheRedshirt <97266008+ErikTheRedshirt@users.noreply.github.com>
2022-04-14 11:05:05 -0700

    Update Users with top recoverables folder.ps1
    Added note about the usability of this script.

--- Users with top recoverables folder.ps1
+++ Users with top recoverables folder.ps1
@@ -1,2 +1,4 @@
-﻿$count
+##This one is still in the works. I haven't gotten it to work just yet, but it may be becasue the server was just too big. ¯\_(ツ)_/¯
+
+$count
 Get-Mailbox -ResultSize Unlimited -Filter "IsMailboxEnabled '$true'" | Get-MailboxFolderStatistics -FolderScope RecoverableItems | Sort-Object TotalItemSize -Descending | Select-Object DisplayName,TotalItemSize -First $count | Export-CSV .\topmailboxes.csv
